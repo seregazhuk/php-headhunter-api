60985cd607a89485eceb1dca3fa98df687e8e6ac
Zhuk Sergey <seregazhuk88@gmail.com>
2017-04-29 09:57:59 +0300

    add: Comments edit, create, delete

---
+++ src/EndPoints/Dictionaries.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace seregazhuk\HeadHunterApi\EndPoints;
+
+use seregazhuk\HeadHunterApi\Traits\HasAll;
+
+class Dictionaries extends Endpoint
+{
+    const RESOURCE = 'dictionaries';
+
+    use HasAll;
+}
