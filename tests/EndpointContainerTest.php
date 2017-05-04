11a744f870cbdaed9c8573cf13ba379f8973c5ff
Zhuk Sergey <seregazhuk88@gmail.com>
2017-05-04 20:01:20 +0300

    upd: tests

--- tests/EndpointContainerTest.php
+++ tests/EndpointContainerTest.php
@@ -3,22 +3,29 @@
 namespace seregazhuk\tests;
 
 use Mockery;
+use Mockery\MockInterface;
 use PHPUnit_Framework_TestCase;
-use seregazhuk\HeadHunterApi\Contracts\RequestInterface;
-use seregazhuk\HeadHunterApi\EndPoints\EndpointsContainer;
 use seregazhuk\HeadHunterApi\Request;
+use seregazhuk\HeadHunterApi\EndPoints\EndpointsContainer;
+use seregazhuk\HeadHunterApi\Exceptions\WrongEndPointException;
 
 class EndpointsContainerTest extends PHPUnit_Framework_TestCase
 {
     /**
      * @var EndpointsContainer
      */
-    private $container;
+    protected $container;
+
+    /**
+     * @var Request|MockInterface
+     */
+    protected $request;
+
     public function setUp()
     {
         /** @var Request $request */
-        $request = Mockery::mock(Request::class);
-        $this->container = new EndpointsContainer($request);
+        $this->request = Mockery::mock(Request::class);
+        $this->container = new EndpointsContainer($this->request);
     }
 
     public function tearDown() {
@@ -28,15 +35,28 @@ class EndpointsContainerTest extends PHPUnit_Framework_TestCase
     /** @test */
     public function getValidProvider()
     {
-        $provider = $this->container->getEndpoint('vacancies');
+        $provider = $this->container->vacancies;
         $this->assertNotEmpty($provider);
     }
+
     /**
      * @test
-     * @expectedException \seregazhuk\HeadHunterApi\Exceptions\WrongEndPointException
      */
     public function getWrongProvider()
     {
+        $this->expectException(WrongEndPointException::class);
         $this->container->getEndpoint('unknown');
     }
+
+    /** @test */
+    public function it_delegates_setters_to_request_object()
+    {
+        $host = 'value';
+        $this->request->shouldReceive('setHost')
+            ->once()
+            ->with($host);
+
+        $this->container->setHost($host);
+    }
 }
+
