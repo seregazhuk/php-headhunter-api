1b3fb9937a0fe5529eb6c55392dd16f8b22ae16d
Zhuk Sergey <seregazhuk88@gmail.com>
2017-05-13 20:36:14 +0300

    add: resume black/white lists refactoring

--- src/EndPoints/Resumes.php
+++ src/EndPoints/Resumes.php
@@ -3,14 +3,18 @@
 namespace seregazhuk\HeadHunterApi\EndPoints;
 
 use seregazhuk\HeadHunterApi\Traits\HasView;
+use seregazhuk\HeadHunterApi\Traits\HasVisibilityList;
 use seregazhuk\HeadHunterApi\Traits\HasSimilarVacancies;
 
 class Resumes extends Endpoint
 {
     const RESOURCE = 'resumes';
 
-    use HasView, HasSimilarVacancies;
+    use HasView, HasSimilarVacancies, HasVisibilityList;
 
+    /**
+     * @return array
+     */
     public function mine()
     {
         return $this->getResource('mine');
@@ -54,8 +58,8 @@ class Resumes extends Endpoint
     }
 
     /**
-     * @param $id
-     * @param $attributes
+     * @param string $id
+     * @param array $attributes
      * @return mixed
      */
     public function edit($id, $attributes)
@@ -64,7 +68,7 @@ class Resumes extends Endpoint
     }
 
     /**
-     * @param $attributes
+     * @param array $attributes
      * @return mixed
      */
     public function create($attributes)
@@ -90,99 +94,12 @@ class Resumes extends Endpoint
         return $this->getSimilarVacanciesFor($id);
     }
 
+    /**
+     * @param string $id
+     * @return array
+     */
     public function negotiations($id)
     {
         return $this->getResource($id . '/negotiations_history');
-    }
-
-    public function getWhiteList($id)
-    {
-        return $this->getResource($id .'/whitelist');
-    }
-
-    public function getBlackList($id)
-    {
-        return $this->getResource($id .'/blacklist');
-    }
-
-    /**
-     * @param string $resumeId
-     * @param string|array $companyId
-     * @return mixed
-     */
-    public function addToWhiteList($resumeId, $companyId)
-    {
-        return $this->addVisibilityItem($resumeId, $companyId, 'whitelist');
-    }
-
-    /**
-     * @param string $resumeId
-     * @param string|array $companyId
-     * @return mixed
-     */
-    public function addToBlackList($resumeId, $companyId)
-    {
-        return $this->addVisibilityItem($resumeId, $companyId, 'blacklist');
-    }
-
-    /**
-     * @param string $resumeId
-     * @param string|array $companyId
-     * @param string $list
-     * @return array
-     */
-    protected function addVisibilityItem($resumeId, $companyId, $list)
-    {
-        $companyId = is_array($companyId) ? $companyId : [$companyId];
-
-        $companies = array_map(
-            function ($company) {
-                return ['id' => (string)$company];
-            }, $companyId
-        );
-
-        return $this->postResourceJson($resumeId. "/" . $list, ['items' => $companies]);
-    }
-
-    /**
-     * @param string $resumeId
-     * @param null $companyId
-     * @return array|null
-     */
-    public function removeFromWhiteList($resumeId, $companyId = null)
-    {
-        $params = $companyId ? ['id' => $companyId] : [];
-
-        return $this->deleteResource($resumeId . '/whitelist/employer', $params);
-    }
-
-    /**
-     * @param string $resumeId
-     * @return array|null
-     */
-    public function clearWhiteList($resumeId)
-    {
-        return $this->removeFromWhiteList($resumeId);
-    }
-
-    /**
-     * @param string $resumeId
-     * @param null $companyId
-     * @return array|null
-     */
-    public function removeFromBlackList($resumeId, $companyId = null)
-    {
-        $params = $companyId ? ['id' => $companyId] : [];
-
-        return $this->deleteResource($resumeId . '/blacklist/employer', $params);
-    }
-
-    /**
-     * @param string $resumeId
-     * @return array|null
-     */
-    public function clearBlackList($resumeId)
-    {
-        return $this->removeFromBlackList($resumeId);
     }
 }
---
+++ src/Traits/HasVisibilityList.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace seregazhuk\HeadHunterApi\Traits;
+
+trait HasVisibilityList
+{
+    /**
+     * @param string $verb
+     * @param array $params
+     * @return mixed
+     */
+    abstract public function getResource($verb = '', array $params = []);
+
+    /**
+     * @param string $verb
+     * @param array $params
+     * @return mixed
+     */
+    abstract protected function postResourceJson($verb = '', array $params = []);
+
+    /**
+     * @param string $verb
+     * @param array $params
+     * @return array|null
+     */
+    abstract protected function deleteResource($verb = '', $params = []);
+
+    /**
+     * @param string $id
+     * @return array
+     */
+    public function getWhiteList($id)
+    {
+        return $this->getResource($id .'/whitelist');
+    }
+
+    /**
+     * @param string $id
+     * @return array
+     */
+    public function getBlackList($id)
+    {
+        return $this->getResource($id .'/blacklist');
+    }
+
+    /**
+     * @param string $resumeId
+     * @param string|array $companyId
+     * @return mixed
+     */
+    public function addToWhiteList($resumeId, $companyId)
+    {
+        return $this->addVisibilityItem($resumeId, $companyId, 'whitelist');
+    }
+
+    /**
+     * @param string $resumeId
+     * @param string|array $companyId
+     * @return mixed
+     */
+    public function addToBlackList($resumeId, $companyId)
+    {
+        return $this->addVisibilityItem($resumeId, $companyId, 'blacklist');
+    }
+
+    /**
+     * @param string $resumeId
+     * @param string|array $companyId
+     * @param string $list
+     * @return array
+     */
+    protected function addVisibilityItem($resumeId, $companyId, $list)
+    {
+        $companyId = is_array($companyId) ? $companyId : [$companyId];
+
+        $companies = array_map(
+            function ($company) {
+                return ['id' => (string)$company];
+            }, $companyId
+        );
+
+        return $this->postResourceJson($resumeId. "/" . $list, ['items' => $companies]);
+    }
+
+    /**
+     * @param string $resumeId
+     * @param string $companyId
+     * @return array|null
+     */
+    public function removeFromWhiteList($resumeId, $companyId)
+    {
+        return $this->deleteResource(
+            $resumeId . '/whitelist/employer',
+            ['id' => $companyId]
+        );
+    }
+
+    /**
+     * @param string $resumeId
+     * @return array|null
+     */
+    public function clearWhiteList($resumeId)
+    {
+        return $this->deleteResource($resumeId . '/whitelist/');
+    }
+
+    /**
+     * @param string $resumeId
+     * @param string $companyId
+     * @return array|null
+     */
+    public function removeFromBlackList($resumeId, $companyId)
+    {
+        return $this->deleteResource(
+            $resumeId . '/blacklist/employer',
+            ['id' => $companyId]
+        );
+    }
+
+    /**
+     * @param string $resumeId
+     * @return array|null
+     */
+    public function clearBlackList($resumeId)
+    {
+        return $this->delete($resumeId . '/blacklist/');
+    }
+}
